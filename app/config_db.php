1ff4ae4684aa715ab4ebdad7f403c55a78558b44
keith quinn <keith quinn>
2016-03-22 23:09:33 +0000

    added gitignore

---
+++ app/config_db.php
@@ -0,0 +1,11 @@
+<?php
+//------- important -------
+// add this file to your .gitignore
+//
+// so we do NOT upload/publish our DB config to the cloud repository!
+//----------------------------
+// constants for our DB configuration
+define('DB_HOST', 'localhost');
+define('DB_USER', 'quincy');
+define('DB_PASS', 'jones');
+define('DB_NAME', 'cdm');
